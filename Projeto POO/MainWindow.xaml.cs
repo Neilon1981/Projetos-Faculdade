66f7fc6b6a850f67ef7a82022914aa981fdbfaea
Haru <109532224+HaruSoftware@users.noreply.github.com>
2026-05-11 22:58:12 -0300

    Favorites

--- Projeto POO/MainWindow.xaml.cs
+++ Projeto POO/MainWindow.xaml.cs
@@ -25,15 +25,25 @@ namespace Projeto_POO
 
         private List<Game> AllGames = new();
 
+        private bool ShowingFavorites = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Adicionar um metodo ou estrutura com varios jogos + link dos jogos para abrir e jogar diretamente.
+            //Todos os jogos devem ser possiveis de jogar através do navegador.
+            //Se necessario, copie as classes ja existentes para novos consoles.
+            //Usar https://www.thecoverproject.net/ para salvar as capas
+            //Manter a estrutura de pastas corretas para cada tipo de console
+            //Ao salvar as capas, edite as propriedades do arquivo
+            //Build Action = Resource
+            //Copy to Output Directory = Copy if newer
 
             Games.Add(new SuperNintendo
             {
                 Name = "Super Mario World",
                 Category = "Aventura",
-                Plataform = GamePlataform.SuperNintendo,
                 Description = "Super Mario World é um clássico do SNES em que Mario e Luigi exploram Dinosaur Land para salvar a Princesa Peach. O jogo introduz Yoshi e traz fases criativas cheias de segredos, sendo um dos plataformas mais influentes e lembrados dos videogames.",
                 RunPath = "https://emulatorgamer.com/pt/games/super-mario-world/play",
                 Size = 0.003,
@@ -44,19 +54,16 @@ namespace Projeto_POO
             {
                 Name = "Grand Theft Auto III",
                 Category = "Ação / Mundo Aberto",
-                Plataform = GamePlataform.PlaystationTwo,
                 Description = "Grand Theft Auto III revolucionou os jogos de mundo aberto, colocando o jogador na cidade de Liberty City em uma experiência livre de exploração, missões criminais e narrativa não linear. Foi o primeiro GTA totalmente 3D, marcando uma nova era para a franquia.",
                 Size = 1,
                 CoverPath = "UI/Images/Games/PS2/gta3.jpg",
                 Installed = false
             });
-            //Set all categories in combobox
-            Videogames.ItemsSource = Enum.GetValues(typeof(GamePlataform));
-            Videogames.SelectedItem = GamePlataform.PlaystationOne;
 
-            Games = new ObservableCollection<Game>(Games.OrderByDescending(x => x.Plataform == (GamePlataform)Videogames.SelectedItem).ToList());
+            Games = new ObservableCollection<Game>(Games.OrderByDescending(x => x.Installed).ToList());
             AllGames = Games.ToList();
 
+            LoadFavorites();
             DataContext = this;
         }
         private void PlayGame_Click(object sender, RoutedEventArgs e)
@@ -107,14 +114,54 @@ namespace Projeto_POO
         }
         private void RefreshUI()
         {
-            DataContext = null;
-            DataContext = this;
+            GameList.Items.Refresh();
+        }
+        private void Favorite_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement element &&
+                element.DataContext is Game game)
+            {
+                game.Favorite = !game.Favorite;
+
+                SaveFavorites();
+
+                RefreshUI();
+            }
+        }
+        private void AllGames_Click(object sender, RoutedEventArgs e)
+        {
+            Games.Clear();
+
+            foreach (Game game in AllGames)
+            {
+                Games.Add(game);
+            }
+
+            RefreshUI();
+        }
+        private void Favorites_Click(object sender, RoutedEventArgs e)
+        {
+            Games.Clear();
+
+            foreach (Game game in AllGames)
+            {
+                if (game.Favorite)
+                {
+                    Games.Add(game);
+                }
+            }
+
+            RefreshUI();
         }
 
-        private void Videogames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void SaveFavorites()
         {
-            Games = new ObservableCollection<Game>(Games.OrderByDescending(x => x.Plataform == (GamePlataform)Videogames.SelectedItem).ToList());
-            AllGames = Games.ToList();
+            //Monte uma estrutura aplicando conceitos em JSON para salvar a lista de favoritos
+        }
+
+        private void LoadFavorites()
+        {
+            //Carregue o arquivo gerado para ler todos os favoritos salvos.
         }
     }
 }
--- Projeto POO/Scripts/Abstract/Game.cs
+++ Projeto POO/Scripts/Abstract/Game.cs
@@ -9,7 +9,7 @@ namespace Projeto_POO.Scripts
         public string Name { get; set; } = string.Empty;
         public string CoverPath { get; set;  } = string.Empty;
         public string RunPath { get; set; } = string.Empty;
-        public GamePlataform Plataform { get; set; }
+        public bool Favorite { get; set; }
         public string Category { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public double Size {  get; set; }
@@ -18,14 +18,5 @@ namespace Projeto_POO.Scripts
         public abstract void Run();
         public abstract void Uninstall();
         public abstract void Install();
-    }
-    public enum GamePlataform
-    {
-        PlaystationOne = 0,
-        PlaystationTwo = 1,
-        SuperNintendo = 2,
-        GameSystem = 3,
-        GameBoy = 4,
-        Nintendo64 = 5,
     }
 }
